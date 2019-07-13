b1ebd51466a0a99121ef3ba7adb14d9089bdafc4
jaurinu <jaurinu@gmail.com>
2019-07-13 13:37:06 -0500

    Se borra url

--- client/main.js
+++ client/main.js
@@ -25,7 +25,7 @@ console.log(url)
 //  };
 //  weatherWidget()
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1cbe1fb2ff4204ca59d47e7383de4031}'
+
 const fetchWidget = () => {
 fetch(url)
  .then(response => response.json())
